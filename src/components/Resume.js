d5a73c92fcbf9ddac990785e238d43a9322ba0fa
Cody Chen <codychen1@gmail.com>
2021-02-28 12:31:57 -0700

    added to resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,6 +11,42 @@ const Portfolio = (props) => {
             <span>Education</span>
           </h1>
         </div>
+        <div className="nine columns main-col">
+          {resumeData.education &&
+            resumeData.education.map((item) => {
+              return (
+                <div className="row item">
+                  <div className="twelve columns">
+                    <h3>{item.UniversityName}</h3>
+                    <p className="info">{item.specialization}</p>
+                    <p>{item.Achievements}</p>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span>Work</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          {resumeData.work &&
+            resumeData.work.map((item) => {
+              return (
+                <div className="row item">
+                  <div className="twelve columns">
+                    <h3>{item.CompanyName}</h3>
+                    <p className="info">{item.specialization}</p>
+                    <p>{item.Achievements}</p>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </section>
   );
