e2ff0b1782de071038ac233d023d005e726ba027
Cody Chen <codychen1@gmail.com>
2021-02-28 11:41:09 -0700

    added to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import resumeData from "./resumeData";
 
-const App = () => <PortfolioContainer />;
+function App() {
+  return (
+    <div className="App">
+      <Header resumeData={resumeData} />
+      <About resumeData={resumeData} />
+      <Resume resumeData={resumeData} />
+      <Portfolio resumeData={resumeData} />
+      <Contact resumeData={resumeData} />
+      <Footer resumeData={resumeData} />
+    </div>
+  );
+}
 
 export default App;
