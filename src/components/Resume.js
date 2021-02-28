cd56527c95423b6ef3fe636acc341b9370fedf51
Cody Chen <codychen1@gmail.com>
2021-02-28 12:27:02 -0700

    added to resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,17 @@ import React from "react";
 const Portfolio = (props) => {
   let resumeData = props.resumeData;
 
-  return <section id="resume"></section>;
+  return (
+    <section id="resume">
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span>Education</span>
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
