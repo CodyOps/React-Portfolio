e5bc98dc80a88a688c49c5ea9a5ccd918d2eed9b
Cody Chen <codychen1@gmail.com>
2021-02-28 11:29:37 -0700

    added export to resumedata.js

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,110 +1,114 @@
 let resumeData = {
-    "imagebaseurl":"https://codyops.github.io/",
-    "name": "Cody Chen",
-    "role": "Full Stack Developer",
-    "linkedinId":"cody-chen-0b05721b5",
-    "email": "codychen1@gmail.com",
-    "roleDescription": "I am a Full Stack Developer Student at the University of Denver.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/cody-chen-0b05721b5",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/codyops",
-          "className":"fa fa-github"
-        },
-      ],
-      "aboutme":"My name is Cody and I am currently a student in the full stack software development program at the University of Denver.",
-      "address":"Colorado",
-      "website":"https://codyops.github.io",
-      "education":[
-        {
-          "UniversityName":"The University of Colorado Bouler",
-          "Specialization":"Integrative Physiology",
-          "MonthOfGrad":"May",
-          "YearOfGrad":"2018",
-          "Achievements":"Summa Cum Laude"
-        },
-        {
-          "UniversityName":"University of Denver",
-          "Specialization":"Full Stack Developer",
-          "MonthOfGrad":"March",
-          "YearOfGrad":"2021",
-          "Achievements":"MERN Developer"
-        }
-      ],
-      "work":[
-        {
-          "CompanyName":"Company One",
-          "Specialization":"Some specialization",
-          "MonthOfLeaving":"Jan",
-          "YearOfLeaving":"2021",
-          "Achievements":"Some Achievements"
-        },
-        {
-          "CompanyName":"Company Two",
-          "Specialization":"Some specialization",
-          "MonthOfLeaving":"Jan",
-          "YearOfLeaving":"2021",
-          "Achievements":"Some Achievements"
-        }
-      ],
-      "skillsDescription":"My skills are Front and Back End Development.",
-      "skills":[
-        {
-          "skillname":"HTML5"
-        },
-        {
-          "skillname":"CSS"
-        },
-        {
-          "skillname":"Javascript"
-        },
-        {
-            "skillname":"Node.js"
-          },
-          {
-            "skillname":"Express.js"
-          },
-          {
-            "skillname":"MySQL"
-          },
-          {
-            "skillname":"Sequelize.js"
-          },
-          {
-            "skillname":"MongoDB"
-          },
-          {
-            "skillname":"Mongoose ORM"
-          },
-          {
-            "skillname":"React.js"
-          },
+  imagebaseurl: "https://codyops.github.io/",
+  name: "Cody Chen",
+  role: "Full Stack Developer",
+  linkedinId: "cody-chen-0b05721b5",
+  email: "codychen1@gmail.com",
+  roleDescription:
+    "I am a Full Stack Developer Student at the University of Denver.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/cody-chen-0b05721b5",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/codyops",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "My name is Cody and I am currently a student in the full stack software development program at the University of Denver.",
+  address: "Colorado",
+  website: "https://codyops.github.io",
+  education: [
+    {
+      UniversityName: "The University of Colorado Bouler",
+      Specialization: "Integrative Physiology",
+      MonthOfGrad: "May",
+      YearOfGrad: "2018",
+      Achievements: "Summa Cum Laude",
+    },
+    {
+      UniversityName: "University of Denver",
+      Specialization: "Full Stack Developer",
+      MonthOfGrad: "March",
+      YearOfGrad: "2021",
+      Achievements: "MERN Developer",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Company One",
+      Specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2021",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Company Two",
+      Specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2021",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "My skills are Front and Back End Development.",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Javascript",
+    },
+    {
+      skillname: "Node.js",
+    },
+    {
+      skillname: "Express.js",
+    },
+    {
+      skillname: "MySQL",
+    },
+    {
+      skillname: "Sequelize.js",
+    },
+    {
+      skillname: "MongoDB",
+    },
+    {
+      skillname: "Mongoose ORM",
+    },
+    {
+      skillname: "React.js",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/weather.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/cat.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "",
+    },
+  ],
+};
 
-      ],
-      "portfolio":[
-        {
-          "name":"project1",
-          "description":"mobileapp",
-          "imgurl":"images/portfolio/weather.jpg"
-        },
-        {
-          "name":"project2",
-          "description":"mobileapp",
-          "imgurl":"images/portfolio/cat.jpg"
-        },
-        {
-          "name":"project3",
-          "description":"mobileapp",  
-          "imgurl":""
-        },
-        {
-          "name":"project4",
-          "description":"mobileapp",
-          "imgurl":""
-        }
-      ],
+export default resumeData;
