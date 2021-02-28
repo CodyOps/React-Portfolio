a401d3c0250e21870e5487e74e33eb03e2a6ba3c
Cody Chen <codychen1@gmail.com>
2021-02-28 13:57:05 -0700

    added container to js files

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,38 +1,41 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 
 const Portfolio = (props) => {
   let resumeData = props.resumeData;
   return (
-    <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Feel free to view some of my project collections.</h1>
-          <div
-            id="portfolio-wrapper"
-            className="bgrid-quarters s-bgrid-thirds cf"
-          >
-            {resumeData.portfolio &&
-              resumeData.portfolio.map((item) => {
-                return (
-                  <div className="columns portfolio-item">
-                    <div className="item-wrap">
-                      <a href="#modal-01">
-                        <img src={`${item.imgurl}`} className="item-img" />
-                        <div className="overlay">
-                          <div className="portfolio-item-meta">
-                            <h5>{item.name}</h5>
-                            <p>{item.description}</p>
+    <Container>
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Feel free to view some of my project collections.</h1>
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {resumeData.portfolio &&
+                resumeData.portfolio.map((item) => {
+                  return (
+                    <div className="columns portfolio-item">
+                      <div className="item-wrap">
+                        <a href="#modal-01">
+                          <img src={`${item.imgurl}`} className="item-img" />
+                          <div className="overlay">
+                            <div className="portfolio-item-meta">
+                              <h5>{item.name}</h5>
+                              <p>{item.description}</p>
+                            </div>
                           </div>
-                        </div>
-                      </a>
+                        </a>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Container>
   );
 };
 
