a65aa8d0cd319bf60c6c1ada48c777be6cc2917c
Cody Chen <codychen1@gmail.com>
2021-02-28 11:16:56 -0700

    added object data to resumeData.js

---
+++ src/resumeData.js
@@ -0,0 +1,19 @@
+let resumeData = {
+    "imagebaseurl":"https://codyops.github.io/",
+    "name": "Cody Chen",
+    "role": "Full Stack Developer",
+    "linkedinId":"cody-chen-0b05721b5",
+    "email": "codychen1@gmail.com",
+    "roleDescription": "I am a Full Stack Developer Student at the University of Denver.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/cody-chen-0b05721b5",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/codyops",
+          "className":"fa fa-github"
+        },
+      ],
