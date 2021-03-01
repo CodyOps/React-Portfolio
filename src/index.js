0378f1f0d8b1aa90a26726f76d37d080991e9cb9
Cody Chen <codychen1@gmail.com>
2021-03-01 09:23:28 -0700

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<BrowserRouter>
+    <App />
+  </BrowserRouter>,, document.getElementById("root"));
